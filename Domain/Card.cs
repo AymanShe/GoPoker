0b7d33de73cdc2216f16d6702e6d0c0b7d5556af
AymanShe <aymalsh@gmail.com>
2023-04-04 23:36:03 -0400

    Add Card class

---
+++ Domain/Card.cs
@@ -0,0 +1,16 @@
+﻿using static Domain.Enums;
+
+namespace Domain
+{
+    public struct Card
+    {
+        public Suit Suit { get; }
+        public Value Value { get; }
+
+        public Card(Suit suit, Value value)
+        {
+            Suit = suit;
+            Value = value;
+        }
+    }
+}
---
+++ Domain/Enums.cs
@@ -0,0 +1,30 @@
+﻿namespace Domain
+{
+    public static class Enums
+    {
+        public enum Suit
+        {
+            Hearts,
+            Spades,
+            Clubs,
+            Diamonds
+        }
+
+        public enum Value
+        {
+            Ace = 1,
+            Two = 2,
+            Three = 3,
+            Four = 4,
+            Five = 5,
+            Six = 6,
+            Seven = 7,
+            Eight = 8,
+            Nine = 9,
+            Ten = 10,
+            Jack = 11,
+            Queen = 12,
+            King = 13
+        }
+    }
+}
