6e4a91cc3af01324f2f0e8c54453e3c8c7cb1346
AymanShe <aymalsh@gmail.com>
2023-04-05 19:37:17 -0400

    Create Player Interfaces

---
+++ Application/Interfaces/IPlayerService.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+
+namespace Application.Interfaces
+{
+    public interface IPlayerService
+    {
+        Player CreatePlayer(Player player);
+        Player GetPlayerById(int id);
+        IList<Player> GetAllPlayers();
+        IList<Player> GetAllPlayersByGameId(int gameId);
+        Player UpdatePlayer(Player player);
+        void DeletePlayer(Player player);
+    }
+}
---
+++ Domain/Interfaces/IPlayerRepository.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+
+namespace Domain.Interfaces
+{
+    public interface IPlayerRepository
+    {
+        Player Add(Player player);
+        Player GetById(int id);
+        IList<Player> GetAll();
+        void Update(Player player);
+        void Delete(Player player);
+    }
+}
